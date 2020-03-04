c19446c7d0023f47dbc6ddcd41c084803338ce47
Hubert Grossin <hubert.grossin@gmail.com>
2020-03-04 16:49:59 +0100

    Moved scripts into separate folder

---
+++ Assets/_GAME/Scripts/Movement.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public enum EChangeOrientationAxis
+{
+    None,
+    TransformRight,
+    TransformForward
+}
+
+///<summary>
+/// Allow a character to move.
+///</summary>
+public class Movement : MonoBehaviour
+{
+
+    [Header("Settings")]
+
+    [SerializeField]
+    private float m_Speed = 8f;
+
+    [SerializeField]
+    private EChangeOrientationAxis m_OrientationVector = EChangeOrientationAxis.TransformRight;
+
+    [SerializeField]
+    private LayerMask m_ObstaclesDetection = ~0;
+
+    [SerializeField]
+    private BoxCollider m_Collider = null;
+
+    [Header("Events")]
+
+    [SerializeField]
+    private UnityEvent m_OnBeginMove = new UnityEvent();
+
+    [SerializeField]
+    private MovementInfosEvent m_OnUpdateMove = new MovementInfosEvent();
+
+    [SerializeField]
+    private UnityEvent m_OnStopMove = new UnityEvent();
+
+    [SerializeField]
+    private Vector3Event m_OnChangeOrientation = new Vector3Event();
+
+    private Vector3 m_LastOrientation = Vector3.zero;
+    private bool m_MovedLastFrame = false;
+
+    private void Awake()
+    {
+        if (m_Collider == null) { m_Collider = GetComponent<BoxCollider>(); }
+    }
+
+    public bool Move(Vector3 _Direction, float _DeltaTime)
+    {
+        if (_Direction == Vector3.zero)
+        {
+            if (m_MovedLastFrame)
+            {
+                m_MovedLastFrame = false;
+                m_OnStopMove.Invoke();
+            }
+
+            return false;
+        }
+
+        if (!m_MovedLastFrame)
+        {
+            m_MovedLastFrame = true;
+            m_OnBeginMove.Invoke();
+        }
+
+        Vector3 lastPosition = transform.position;
+        Vector3 targetPosition = lastPosition;
+
+        float castDistance = m_Speed * Mathf.Abs(_Direction.x) * Time.deltaTime;
+        // If no obstacles hit
+        if (!Physics.BoxCast(transform.position, Extents, Orientation, Quaternion.identity, castDistance, m_ObstaclesDetection))
+        {
+            // Change target position
+            targetPosition = transform.position + _Direction * m_Speed * _DeltaTime;
+        }
+
+        transform.position = targetPosition;
+        ChangeOrientation(_Direction);
+        m_OnUpdateMove.Invoke(new MovementInfos { speed = m_Speed, lastPosition = lastPosition, currentPosition = targetPosition });
+
+        return true;
+    }
+
+    private void ChangeOrientation(Vector3 _Direction)
+    {
+        Vector3 orientation = _Direction;
+        orientation.Normalize();
+
+        if (orientation == m_LastOrientation)
+        {
+            return;
+        }
+
+        switch (m_OrientationVector)
+        {
+            case EChangeOrientationAxis.TransformForward:
+                transform.forward = orientation;
+                break;
+
+            case EChangeOrientationAxis.TransformRight:
+                transform.right = orientation;
+                break;
+
+            default:
+                break;
+        }
+
+        m_OnChangeOrientation.Invoke(orientation);
+        m_LastOrientation = orientation;
+    }
+
+    private Vector3 Orientation
+    {
+        get
+        {
+            Vector3 orientation = transform.forward;
+            switch (m_OrientationVector)
+            {
+                case EChangeOrientationAxis.TransformRight:
+                    orientation = transform.right;
+                    break;
+
+                default:
+                    break;
+            }
+            return orientation;
+        }
+
+
+    }
+
+    private Vector3 Extents
+    {
+        get
+        {
+            if (m_Collider != null)
+            {
+                return m_Collider.bounds.extents;
+            }
+            return Vector3.one / 2;
+        }
+    }
+
+    private Vector3 Size
+    {
+        get
+        {
+            if (m_Collider != null)
+            {
+                return m_Collider.bounds.size;
+            }
+            return Vector3.one;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawWireCube(transform.position + Orientation * m_Speed * Time.deltaTime, Size);
+    }
+
+}
---
+++ Assets/_GAME/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+///<summary>
+/// 
+///</summary>
+public class PlayerController : MonoBehaviour
+{
+    [Header("Settings")]
+
+    [SerializeField]
+    private float m_JumpForce = 12f;
+
+    [Header("References")]
+
+    [SerializeField]
+    private Movement m_Movement = null;
+
+    [SerializeField]
+    private Rigidbody m_Rigidbody = null;
+
+    private void Awake()
+    {
+        if(m_Movement == null) { m_Movement = GetComponent<Movement>(); }
+        if(m_Rigidbody == null) { m_Rigidbody = GetComponent<Rigidbody>(); }
+    }
+
+	private void Update()
+    {
+        CheckMovement();
+        CheckJump();
+    }
+
+    private void CheckMovement()
+    {
+        float hMovement = Input.GetAxis("Horizontal");
+        Vector3 movement = Vector3.right * hMovement;
+
+        m_Movement.Move(movement, Time.deltaTime);
+    }
+
+    private void CheckJump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            m_Rigidbody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
+        }
+    }
+
+}
