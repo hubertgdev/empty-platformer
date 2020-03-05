f7cf17c2ee9627f60718b2a1b7809c1a705893ac
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-03-06 00:47:00 +0100

    Add Logger, and setup basic events logs

---
+++ Assets/_GAME/Scripts/Utilities/Logger.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+///<summary>
+/// A utility ScriptableObject that can be created as an asset to be used as a Debug.Log() method without opening a code editor.
+/// For example, this asset can be useful for logging informations from UnityEvents.
+///</summary>
+[CreateAssetMenu(fileName = "NewLogger", menuName = "Utility/Logger")]
+public class Logger : ScriptableObject
+{
+
+    #region Properties
+
+    [SerializeField]
+    private string m_Prefix = string.Empty;
+
+    [SerializeField]
+    private string m_Suffix = string.Empty;
+
+    #endregion
+
+
+    #region Static Log Methods
+
+    /// <summary>
+    /// Logs the given message.
+    /// </summary>
+    public void LogMessage(string _Message)
+    {
+        Debug.Log(_Message);
+    }
+
+    #endregion
+
+
+    #region Dynamic Log Methods
+
+    /// <summary>
+    /// Logs the defined prefix and suffix.
+    /// </summary>
+    public void Log()
+    {
+        Debug.Log($"{m_Prefix} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(string _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(int _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(float _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(bool _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(Vector3 _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(Quaternion _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    /// <summary>
+    /// Logs the given information, adding the defined prefix and suffix.
+    /// </summary>
+    public void Log(GameObject _Data)
+    {
+        Debug.Log($"{m_Prefix} {_Data} {m_Suffix}");
+    }
+
+    #endregion
+
+}
