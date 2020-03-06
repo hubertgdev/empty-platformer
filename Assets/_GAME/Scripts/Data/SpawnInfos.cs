f51477704cc31b5eb0b21f77ac6ceb5aaf980469
Hubert Grossin <hubert.grossin@gmail.com>
2020-03-06 11:40:05 +0100

    Add Respawn mechanic and setup Respawn events

---
+++ Assets/_GAME/Scripts/Data/SpawnInfos.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+///<summary>
+/// Contains respawn informations.
+///</summary>
+[System.Serializable]
+public struct SpawnInfos
+{
+	public Vector3 lastPosition;
+	public Vector3 spawnPosition;
+}
