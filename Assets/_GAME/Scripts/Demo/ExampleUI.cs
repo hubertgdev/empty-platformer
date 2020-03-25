9f759f4ce8d60729faff450007c4f52ab10a95e5
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-03-25 10:09:57 +0100

    Add demo UI for player lives and score

---
+++ Assets/_GAME/Scripts/Demo/ExampleUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+///<summary>
+/// 
+///</summary>
+public class ExampleUI : MonoBehaviour
+{
+
+    private const string LIVES_PREFIX = "Remaining lives: ";
+    private const string SCORE_PREFIX = "Score: ";
+
+    [SerializeField]
+    private Text m_LivesCount = null;
+
+    [SerializeField]
+    private Text m_Score = null;
+
+    public void UpdateHealth(DamagesInfos _Infos)
+    {
+        m_LivesCount.text = LIVES_PREFIX + _Infos.remainingLives;
+    }
+
+    public void UpdateHealth(Health _Health)
+    {
+        m_LivesCount.text = LIVES_PREFIX + _Health.RemainingLives;
+    }
+
+    public void UpdateScore(ScoringInfos _Infos)
+    {
+        m_Score.text = SCORE_PREFIX + _Infos.score;
+    }
+
+}
