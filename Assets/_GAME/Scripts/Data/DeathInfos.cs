f5ea6d3ea1675a9d6b1cb87b25787ec930276a66
Hubert GROSSIN <hubert.grossin@gmail.com>
2020-12-11 21:55:13 +0100

    Improve OnDie event

---
+++ Assets/_GAME/Scripts/Data/DeathInfos.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+///<summary>
+/// Contains informations about an entity that has just died.
+///</summary>
+[System.Serializable]
+public struct DeathInfos
+{
+    // The entity that has just died.
+    public GameObject dead;
+
+    // The position where the entity died.
+    public Vector3 position;
+}
